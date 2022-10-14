b33b64a2a2eb30db211cffa92448361dd53b1d64
Govindarajan D <rdgovindarajan@gmail.com>
2022-10-14 21:44:28 +0530

    Initialized Menubar

--- gui/pv_application.py
+++ gui/pv_application.py
@@ -1,26 +1,48 @@
+import os
+
 from PySide6 import QtCore, QtWidgets
-from PySide6.QtWidgets import QMainWindow, QMenuBar, QMenu
+from PySide6.QtSvg import QSvgRenderer
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMainWindow, QMenuBar, QMenu, QFileDialog
 import sys
+import os.path as path
 
 
 class PVApplication(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("PythonView")
         self.AppName = "PythonView Application"
-        self.__menu_bar_init()
+        self.svg_resource_path = path.abspath(path.join(__file__, os.pardir, os.pardir, "icons"))
+        self._create_actions()
+        self._menu_bar_init()
+        self._connect_actions()
 
-    def __menu_bar_init(self):
+    def _menu_bar_init(self):
         main_menu_bar = QMenuBar(self)
+
         file_menu = QMenu("&File", self)
+        file_menu.addAction(self.new_action)
+
         help_menu = QMenu("&Help", self)
+
         main_menu_bar.addMenu(file_menu)
         main_menu_bar.addMenu(help_menu)
 
         self.setMenuBar(main_menu_bar)
 
+    def _create_actions(self):
+        self.new_action = QAction(QIcon(self.svg_resource_path), "&Open", self)
+        self.new_action.setShortcut("Ctrl+O")
+        self.new_action.setToolTip("Open existing file")
+
+    def _connect_actions(self):
+        self.new_action.triggered.connect(self.open_file)
+
     @QtCore.Slot()
-    def magic(self):
-        pass
+    def open_file(self):
+        open_selected_file_name = QFileDialog.getOpenFileNames(self, "Open PyView file", os.path.expanduser("~"),
+                                                               'PyView (*.pyv)')
 
 
 if __name__ == "__main__":
