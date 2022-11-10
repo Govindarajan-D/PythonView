914dff3e6aaa1a3443686444fc933c3975886b40
Govindarajan D <rdgovindarajan@gmail.com>
2022-11-10 22:54:39 +0530

    Core: PandasGenerator logic in Progress - Filepath basename derived for Dataframe names

--- core/pandas_dataframes.py
+++ core/pandas_dataframes.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from core.pandas_generator import PandasGenerator
+import ntpath
 
 
 class DataFrame:
-    def __init__(self, file_name, init_type):
+    def __init__(self, file_path, init_type):
         match init_type:
             case 'csv':
-                self.pandas_df = pd.read_csv(file_name)
-                self.gen_object = PandasGenerator()
-                operation_data = {'file_name': file_name}
-                self.gen_object.generate_script("read_csv", file_name, operation_data)
+                self.pandas_df = pd.read_csv(file_path)
+                dataframe_name = ntpath.basename(file_path).replace(".","_")
+                self.gen_object = PandasGenerator(dataframe_name)
+                operation_data = {'file_path': file_path}
+                self.gen_object.generate_script("read_csv", operation_data)
--- core/pandas_generator.py
+++ core/pandas_generator.py
@@ -2,19 +2,19 @@ from core.data_structures import TextStack
 
 
 class PandasGenerator:
-    data_frame_names = ["data_frame"]
     transform_code = TextStack()
 
-    def __init__(self):
+    def __init__(self, dataframe_name):
         self.table_script = ""
+        self.dataframe_name = dataframe_name
 
-    def generate_script(self, operation_type, dataframe_name, operation_data):
+    def generate_script(self, operation_type, operation_data):
         match operation_type:
             case "new_column_addition":
-                self.table_script = dataframe_name + "[" + "test" + "] = " + operation_data
+                self.table_script = self.dataframe_name + "[" + operation_data['new_column_name'] + "] = " + operation_data['operation']
             case "select_columns":
-                self.table_script += dataframe_name + "[[" + operation_data + "]]"
+                self.table_script += self.dataframe_name + "[[" + operation_data['columns'] + "]]"
             case "read_csv":
-                self.table_script += "pd.read_csv(" + operation_data['file_name'] + ")"
+                self.table_script += "pd.read_csv(" + operation_data['file_path'] + ")"
 
         self.transform_code.add_step(self.table_script)
