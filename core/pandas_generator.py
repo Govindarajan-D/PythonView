197092baa5a1acea62323b086a9c4d22c3f82349
Govindarajan D <rdgovindarajan@gmail.com>
2022-11-12 23:59:11 +0530

    Core: PandasGenerator - Dataframe names logic - Text stack in progress

--- core/pandas_generator.py
+++ core/pandas_generator.py
@@ -9,12 +9,20 @@ class PandasGenerator:
 
     def generate_script(self, operation_type, operation_data):
         current_df_name = self.step_df_names.peek()
+        if current_df_name is None:
+            current_df_name = self.final_dataframe_name
+            self.step_df_names.add_step(current_df_name)
         match operation_type:
             case "new_column_addition":
-                table_script = self.operation_type + "[" + operation_data['new_column_name'] + "] = " + operation_data['operation']
+                table_script = operation_type + "[" + operation_data['new_column_name'] + "] = " + operation_data['operation']
             case "select_columns":
-                table_script = self.operation_type + " = " + self.current_df_name + "[[" + operation_data['columns'] + "]]"
+                table_script = operation_type + " = " + current_df_name + "[[" + operation_data['columns'] + "]]"
             case "read_csv":
-                table_script = "pd.read_csv(" + operation_data['file_path'] + ")"
+                table_script = operation_type + " = " + "pd.read_csv(" + operation_data['file_path'] + ")"
+
         self.step_df_names.add_step(operation_type)
+        self.transform_code.remove_step()
         self.transform_code.add_step(table_script)
+
+        last_step = self.final_dataframe_name + " = " + operation_type
+        self.transform_code.add_step(last_step)
